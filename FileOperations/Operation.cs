c0235674286132d2ec8eb2f91a349b59cc6c948c
somu3341 <somashekarvsv1996@gmail.com>
2023-04-09 19:03:43 +0530

    [somu]Add:Checking File Exits or Not.

---
+++ FileOperations/Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOperations
+{
+    public class Operation
+    {
+        public void FileExits(string filePath)
+        {
+            if (File.Exists(filePath))
+                Console.WriteLine("File Exits");
+            else
+                Console.WriteLine("File not Exits");
+        }
+    }
+}
---
+++ FileOperations/Program.cs
@@ -0,0 +1,27 @@
+﻿using FileOperations;
+using System;
+namespace FileOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to Perform\n1.File Exits\n2.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                Operation operation = new Operation();
+                switch(option)
+                {
+                    case 1:
+                        operation.FileExits(@"D:\BridgeLabs\FileOpeartions\FileOperations\Example.txt");
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }           
+        }
+    }
+}
